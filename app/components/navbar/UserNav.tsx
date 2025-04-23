5ecd2ba933f897f7dc774d0c8eb850dee8fb3ccd
prteodoro <97711803+prteodoro@users.noreply.github.com>
2025-04-23 00:04:42 -0300

    Log out

--- app/components/navbar/UserNav.tsx
+++ app/components/navbar/UserNav.tsx
@@ -3,11 +3,18 @@
 import { useState } from 'react'
 
 import MenuLink from './MenuLink'
+import LogoutButton from '../LogoutButton'
 
 import useLoginModal from '../hooks/useLoginModal'
 import useSignupModal from '../hooks/useSignupModal'
 
-const UserNav = () => {
+interface UserNavProps {
+    userId?: string | null
+}
+
+const UserNav: React.FC<UserNavProps> = ({
+    userId
+}) => {
     const loginModal = useLoginModal()
     const signupModal = useSignupModal()
     const [isOpen, setIsOpen] = useState(false)
@@ -35,21 +42,27 @@ const UserNav = () => {
 
             {isOpen && (
                 <div className='w-[200px] absolute top-[60px] right-0 bg-white border rounded-xl shadow-md flex flex-col'>
-                    <MenuLink 
-                        label='Log in'
-                        onClick={() => {
-                            setIsOpen(false)
-                            loginModal.open()
-                        }}
-                    />
+                    {userId ? (
+                        <LogoutButton />
+                    ) : (
+                        <>
+                            <MenuLink 
+                                label='Log in'
+                                onClick={() => {
+                                    setIsOpen(false)
+                                    loginModal.open()
+                                }}
+                            />
 
-                    <MenuLink
-                        label='Sign up'
-                        onClick={() => {
-                            setIsOpen(false)
-                            signupModal.open()
-                        }}
-                    />
+                            <MenuLink
+                                label='Sign up'
+                                onClick={() => {
+                                    setIsOpen(false)
+                                    signupModal.open()
+                                }}
+                            />
+                        </>
+                    )}
                 </div>
             )}
         </div>
--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,25 +3,45 @@
 import { cookies } from "next/headers"
 
 export async function handleLogin(userId: string, accessToken: string, refreshToken: string) {
-    cookies().set('session_userid', userId, {
+    const cookieStore = await cookies()
+
+    cookieStore.set('session_userid', userId, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
         maxAge: 60 * 60 * 24 * 7, // One week
         path: '/'
     })
 
-    cookies().set('session_access_Token', accessToken, {
+    cookieStore.set('session_access_Token', accessToken, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
         maxAge: 60 * 60, // 60 minutes
         path: '/'
     })
 
-    cookies().set('session_refresh_token', refreshToken, {
+    cookieStore.set('session_refresh_token', refreshToken, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
         maxAge: 60 * 60 * 24 * 7, // One week
         path: '/'
     })
 
+}
+
+export async function resetAuthCookies() {
+    const cookieStore = await cookies()
+
+    cookieStore.set('session_userid', '')
+    cookieStore.set('session_access_token', '')
+    cookieStore.set('session_refresh_token', '')
+}
+
+//
+// Get data
+
+export async function getUserId() {
+    const cookieStore = await cookies()
+
+    const userId = cookieStore.get('session_userid')?.value
+    return userId ? userId : null
 }
