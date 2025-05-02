b89669437d17a620497650a7be224ce7442f96c6
prteodoro <97711803+prteodoro@users.noreply.github.com>
2025-05-02 15:55:29 -0300

    Book property

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -2,9 +2,13 @@ import Image from "next/image";
 import ReservationSidebar from "@/app/components/properties/ReservationSidebar";
 
 import apiService from "@/app/services/apiService";
+import { getUserId } from "@/app/lib/actions";
 
 const PropertyDetailPage = async ({params}: { params: {id: string}}) => {
     const property = await apiService.get(`/api/properties/${params.id}`)
+    const userId = await getUserId()
+
+    console.log('userId', userId)
 
     return (
         <main className="max-w-[1500px] mx-auto px-6 pb-6">
@@ -50,6 +54,7 @@ const PropertyDetailPage = async ({params}: { params: {id: string}}) => {
 
                 <ReservationSidebar
                     property={property}
+                    userId={userId}
                 />
             </div>
         </main>
