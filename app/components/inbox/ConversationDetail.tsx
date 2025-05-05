2ebcf40baad5a2be23d7b2f3df25ec154e1064a8
prteodoro <97711803+prteodoro@users.noreply.github.com>
2025-05-05 18:33:39 -0300

    Make it possible to connect

--- app/components/inbox/ConversationDetail.tsx
+++ app/components/inbox/ConversationDetail.tsx
@@ -1,8 +1,34 @@
 'use client'
 
+import { useEffect } from "react";
 import CustomButton from "../forms/CustomButton";
+import { ConversationType } from "@/app/inbox/page";
+import useWebSocket, {ReadyState} from "react-use-websocket";
 
-const ConversationDetail = () => {
+
+interface ConversationDetailProps {
+    token: string
+    userId: string
+    conversation: ConversationType
+}
+
+const ConversationDetail: React.FC<ConversationDetailProps> = ({
+    userId,
+    token,
+    conversation
+}) => {
+    const myUser = conversation.users?.find((user) => user.id == userId)
+    const otherUser = conversation.users?.find((user) => user.id != userId)
+
+    const { sendJsonMessage, lastJsonMessage, readyState } = useWebSocket(`ws://127.0.0.1:8000/ws/${conversation.id}/?token=${token}`, {
+        share: false,
+        shouldReconnect: () => true,
+    })
+
+    useEffect(() => {
+        console.log('Connection state changed', readyState)
+    }, [readyState])
+
     return (
         <>
             <div className="max-h-[400px] overflow-auto flex flex-col space-y-4">
